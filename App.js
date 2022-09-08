20987bd508e8ed1c2a8e1318e6ced76bff32016f
Palash Mandal <palash15-2092@diu.edu.bd>
2022-09-08 22:04:45 +0600

    React native Template 0.68.0

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <Text>React native Template 0.68.0</Text>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
